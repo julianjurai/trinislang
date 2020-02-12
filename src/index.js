c3acb86908cacb449b73d4ffa0a87681b136eab9
Julian <andrewjurai@gmail.com>
2020-02-11 22:53:30 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,50 +1,68 @@
 import Fuse from "fuse.js";
 import dictionary from "./dictionary";
 
-var fuse = new Fuse(dictionary, {
-  keys: ["slang", "definition"],
-  shouldSort: true
-});
-
-const searcResults = document.getElementById("search-results");
-
-document.getElementById("search-input").onkeyup = function(e) {
-  console.log("searching", e.target.value);
-  const results = fuse.search(e.target.value);
-  console.log("fuse.search(e.target.value)", fuse.search(e.target.value));
-  searcResults.innerHTML = results
-    .slice(0, 20)
-    .map(({ slang, definition }) => `<div>${slang + ": " + definition}</div>`);
-};
-
 class TriniLingo {
   constructor() {
     this.searchInput = document.getElementById("search-input");
     this.searcResults = document.getElementById("search-results");
     this.selectedWordContainer = document.getElementById("selected-word");
-
+    this.searcResults.classList.add("no-content");
+    this.cachedSelectedWordOption =
+      dictionary[Math.floor(Math.random() * dictionary.length)];
     this.fuseInstance = new Fuse(dictionary, {
       keys: ["slang", "definition"],
       shouldSort: true
     });
 
-    this.search.bind(this);
+    this.searchInput.onkeyup = this.search.bind(this);
+
     this.setSelectedWord.bind(this);
+    this.selectResult.bind(this);
     this.setSelectedWord(null);
+    this.setPlaceholder();
   }
 
-  search(searchStr) {
-    const results = this.fuseInstance.search(searchStr).slice(0, 11);
+  search(e) {
+    const results = this.fuseInstance.search(e.target.value).slice(0, 11);
+    this.searcResults.innerHTML = "";
 
-    this.searcResults.innerHTML = results.map(
-      ({ slang, definition }) => `<i>${slang + ": " + definition}</i>`
-    );
+    results.map(option => {
+      const div = document.createElement("div");
+      div.textContent = option.slang;
+      div.dataset.option = option;
+      div.onclick = this.selectResult.bind(this, option);
+      div.classList.add("result-item");
+      this.searcResults.appendChild(div);
+    });
+
+    if (results.length > 0) {
+      this.searcResults.classList.remove("no-content");
+    } else {
+      this.searcResults.classList.add("no-content");
+    }
+  }
+
+  setPlaceholder() {
+    const randIndex = Math.floor(Math.random() * dictionary.length);
+    this.searchInput.placeholder = dictionary[randIndex].slang + "...";
+  }
+
+  selectResult(option) {
+    this.searchInput.value = option.slang;
+    this.setSelectedWord(option);
+    this.searcResults.classList.add("no-content");
   }
 
   setSelectedWord(dictionaryOption = null) {
-    const random = !dictionaryOption;
-    const randIndex = Math.floor(Math.random() * dictionary.length);
-    const selectedOption = random ? dictionary[randIndex] : dictionaryOption;
+    const useCached = !dictionaryOption;
+    const selectedOption = useCached
+      ? this.cachedSelectedWordOption
+      : dictionaryOption;
+
+    useCached
+      ? this.selectedWordContainer.classList.remove("selected")
+      : this.selectedWordContainer.classList.add("selected");
+
     this.selectedWordContainer.innerHTML = `
       <h1>${selectedOption.slang}s</h1>
       <div>${selectedOption.definition}</div>
